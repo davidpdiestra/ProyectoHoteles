8eb2060a67b30059e341acdb28e0ef73d5b385be
David Polo Diestra <129630025+davidpdiestra@users.noreply.github.com>
2025-04-12 16:42:32 -0300

    feat: creacion de entidades (#3)

---
+++ Hoteles.Dominio/Entidades/Recepcionista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoteles.Dominio.Entidades
+{
+    public class Recepcionista
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Hoteles.Dominio/Entidades/TipoHabitacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoteles.Dominio.Entidades
+{
+    public class TipoHabitacion
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public int Capacidad { get; set; }
+    }
+}
